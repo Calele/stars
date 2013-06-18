7eae598fdf75c064f4bc29e02a033aace4cf72f7
calele <calel@bk.ru>
2013-06-18 18:47:09 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stars::Application.config.secret_token = '6ad3bd37fecb69bb63e7c52c0312164fff22d2a27ec8ea018527f8475e000053d1942039aef83bb0e47c82b3e67ed10524451290321a7d82b58e43cd0265f593'
